377dd9c3be7d80b8531fb36a4ecf4618806784ef
Wellington Mitrut <wellingtonmitrut@gmail.com>
2015-04-10 16:41:07 -0300

    Fazendo telas para mostrar via modal

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -87,3 +87,17 @@ $(document).ready(function(){
     });
   });
 });
+
+
+function centerModal() {
+  $(this).css('display', 'block');
+  var $dialog = $(this).find(".modal-dialog");
+  var offset = ($(window).height() - $dialog.height()) / 0.5;
+  // Center modal vertically in window
+  $dialog.css("margin-top", offset);
+}
+
+$('.modal').on('show.bs.modal', centerModal);
+$(window).on("resize", function () {
+  $('.modal:visible').each(centerModal);
+});
